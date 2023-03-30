c7f47093addf9c49d6911c7a5ab2d8ec215d335f
Megha <megha13rai@gmail.com>
2023-03-30 18:55:03 +0530

    added details page

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,3 +5,7 @@ def index(request):
     products = Product.objects.all()
 
     return render(request,'myapp/index.html',{'products':products})
+
+def detail(request,id):
+    product=Product.objects.get(id=id)
+    return render(request,'myapp/detail.html',{'product':product})
